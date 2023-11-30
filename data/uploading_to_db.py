04e0c77726ec36a7c91ee9a5d5347639ef30eb26
Селезнев Василий <seleznev.2016@yandex.ru>
2023-12-01 00:12:58 +0300

    Update uploading_to_db.py

--- data/uploading_to_db.py
+++ data/uploading_to_db.py
@@ -1,15 +1,15 @@
 import csv, sqlite3
 
 
-conn = sqlite3.connect("db.sqlite3")
+conn = sqlite3.connect('db.sqlite3')
 curs = conn.cursor()
 with open('ingredients.csv', encoding='utf-8') as r_file:
     reader = csv.reader(r_file, delimiter=',')
-    count = 1
+    id = 1
     for row in reader:
-        to_db = (count, row[0], row[1])
-        curs.execute("INSERT INTO recipes_ingredient VALUES (?, ?, ?);", to_db)
-        count += 1
+        to_db = (id, row[0], row[1])
+        curs.execute('INSERT INTO recipes_ingredient VALUES (?, ?, ?);', to_db)
+        id += 1
 
     conn.commit()
     conn.close()
